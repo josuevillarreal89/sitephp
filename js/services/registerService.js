e703b7df7d9de06daa6bf1fa8afd30de7172b393
Josue Alejandro Silva <josue.villarreal89@outlook.com>
2016-05-03 15:52:11 -0500

    ...

--- js/services/registerService.js
+++ js/services/registerService.js
@@ -2,7 +2,7 @@
 app.factory( 'RegisterService', function($http) {
     return {
         register: function( user ) {
-            var $promise = $http.post( 'http://192.168.56.101/sporify/php/register.php', user );
+            var $promise = $http.post( 'php/register.php', user );
             $promise
                 .then( function( result ) {
                     console.log( result );
